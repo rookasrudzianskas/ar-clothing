67b7abedf2277b07aee9056c369aa1bef086effe
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 10:00:16 +0300

    Add framer-motion and valtio imports and animations to Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { CustomButton } from '../components';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
 
 const Home = () => {
 
   return (
-    <div></div>
+    <div>
+
+    </div>
   )
 }
 
