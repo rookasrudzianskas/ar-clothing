3ad052f30d8bbf2143351af9aca0bdf8aee20cfb
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 11:35:59 +0300

    Add server files and dependencies for DALL.E project

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
+
+export default router;
