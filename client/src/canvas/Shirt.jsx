80831294f07a1ecf875d6a69a670cd4b9d00f34c
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 10:34:21 +0300

    Refactor Shirt.jsx to remove commented out code and simplify mesh component

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -13,19 +13,15 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
-
-  const stateString = JSON.stringify(snap);
-
   return (
     <group key={stateString}>
-      {/*<mesh*/}
-      {/*  castShadow*/}
-      {/*  geometry={nodes.T_Shirt_male.geometry}*/}
-      {/*  material={materials.lambert1}*/}
-      {/*  material-roughness={1}*/}
-      {/*  dispose={null}*/}
-      {/*>*/}
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
       {/*  {snap.isFullTexture && (*/}
       {/*    <Decal */}
       {/*      position={[0, 0, 0]}*/}
@@ -46,7 +42,7 @@ const Shirt = () => {
       {/*      depthWrite={true}*/}
       {/*    />*/}
       {/*  )}*/}
-      {/*</mesh>*/}
+      </mesh>
     </group>
   )
 }
