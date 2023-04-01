6014637b3f843ca733b0dce1b50eed94ebbdbf43
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 10:53:45 +0300

    Refactor Backdrop component to include RandomizedLight component and remove unused imports

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,6 +1,4 @@
 import React, { useRef } from 'react'
-import { easing } from 'maath'
-import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
@@ -24,11 +22,11 @@ const Backdrop = () => {
         position={[5, 5, -10]}
       />
       <RandomizedLight
-      {/*  amount={4}*/}
-      {/*  radius={5}*/}
-      {/*  intensity={0.25}*/}
-      {/*  ambient={0.55}*/}
-      {/*  position={[-5, 5, -9]}*/}
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
       />
     </AccumulativeShadows>
   )
