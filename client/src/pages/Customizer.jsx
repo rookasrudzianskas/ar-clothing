7a2f6a85ec5d4f68adc80afc22590248e23f60bd
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 11:30:23 +0300

    Add AI text generation functionality to AIPicker component and Customizer page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -42,8 +42,31 @@ const Customizer = () => {
     }
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
+    if (!prompt) return alert("Please enter a prompt");
 
+    try {
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
+    }
   }
 
   const handleActiveFilterTab = (tabName) => {
