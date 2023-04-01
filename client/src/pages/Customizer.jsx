a01ca40c336332840ab2b157c631b1e8e2d1ff1a
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 10:17:09 +0300

    Refactor Customizer component to use Valtio state management and Framer Motion for animation

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
-const Customizer = () => {
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
 
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+
+const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
     <div></div>
   )
