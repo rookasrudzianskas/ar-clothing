685d672eb44fbce19b4dd25117a0d1d2dc231180
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 11:15:14 +0300

    "Add file input functionality to FilePicker component"

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -4,7 +4,16 @@ import CustomButton from './CustomButton'
 
 const FilePicker = ({ file, setFile, readFile }) => {
   return (
-    <div></div>
+    <div className="filepicker-container">
+      <div className="flex-1 flex flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+      </div>
+    </div>
   )
 }
 
