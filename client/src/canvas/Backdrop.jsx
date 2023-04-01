63137c8f51ac7961c2ce1d621e292db8529afff1
Rokas <rookas.rudzenskas@gmail.com>
2023-04-01 10:48:07 +0300

    Add randomized lights to Backdrop component for improved lighting

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -16,20 +16,20 @@ const Backdrop = () => {
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
-      {/*<RandomizedLight */}
-      {/*  amount={4}*/}
-      {/*  radius={9}*/}
-      {/*  intensity={0.55}*/}
-      {/*  ambient={0.25}*/}
-      {/*  position={[5, 5, -10]}*/}
-      {/*/>*/}
-      {/*<RandomizedLight */}
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
       {/*  amount={4}*/}
       {/*  radius={5}*/}
       {/*  intensity={0.25}*/}
       {/*  ambient={0.55}*/}
       {/*  position={[-5, 5, -9]}*/}
-      {/*/>*/}
+      />
     </AccumulativeShadows>
   )
 }
